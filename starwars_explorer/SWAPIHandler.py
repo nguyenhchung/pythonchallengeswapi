647f5208c91e6ef5ab5bcfe958ae2a760dea978b
work <work@192.168.0.9>
2022-01-22 19:06:40 +0000

    refactored swapihandler

--- starwars_explorer/SWAPIHandler.py
+++ starwars_explorer/SWAPIHandler.py
@@ -9,42 +9,48 @@ def extract_page_query_param(input_url, query_key):
 
 SWAPI_BASE_URL = 'https://swapi.dev/api/'
 
+class SWAPIResponse:
+    # processing SWAPI specific responses
+    
+    total_list = []
+    total_count = None
+    next_page_token = None
+    
+    def __init__(self, start_response):
+        process(start_response)
+        
+    def add(self, api_response)
+        self.total_count = api_response.get('count', None)
+        self.total_list.extend(api_response.get('results', []))
+        self.next_page_token = api_response.get('next', None)
+
+
 class SWAPIBaseEndpoint:
     endpoint_path = ''
     
     default_payload = {}
     
     def call_endpoint(self, payload=default_payload):
-        
         api_url = SWAPI_BASE_URL + self.endpoint_path
         response = requests.get(api_url, params=payload)
-        
         if response.status_code != 200:
             raise Exception("Unsuccessful API call")
-        else:
-            return response
+        
+        return response
+ 
         
     def get_total_list(self, payload=default_payload):
         total_list = []
         
-        start_response = self.call_endpoint(payload=payload)
-        start_response_json = start_response.json()
+        swapi_response = SWAPIResponse(self.call_endpoint(payload=payload).json())
         
-        total_count = start_response_json.get('count', None)
-        start_response_items = start_response_json.get('results', [])
-        total_list.extend(start_response_items)
-        next_page_token = start_response_json.get('next', None)
-        
-        while(next_page_token):
+        while(swapi_response.next_page_token):
             new_payload = payload
-            next_page = extract_page_query_param(next_page_token, 'page')
-            new_payload['page'] = next_page
-            response = self.call_endpoint(payload=new_payload)
-            response_items = response.json().get('results', [])
-            total_list.extend(response_items)
-            next_page_token = response.json().get('next', None)
+            new_payload['page'] = extract_page_query_param(swapi_response.next_page_token, 'page')
             
-        return total_count, total_list
+            swapi_response.add(self.call_endpoint(payload=new_payload).json())
+
+        return swapi_response.total_count, swapi_response.total_list
         
 class SWAPIPeopleEndpoint(SWAPIBaseEndpoint):
     endpoint_path = 'people/'
